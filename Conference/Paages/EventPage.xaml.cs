4d739759cc4afec2817befed9706b7b970b6e3eb
Салахетдинов Лев Александрович <208521@edu.fa.ru>
2024-04-15 17:22:38 +0300

    first commit

---
+++ Conference/Paages/EventPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Conference.Paages
+{
+    /// <summary>
+    /// Логика взаимодействия для EventPage.xaml
+    /// </summary>
+    public partial class EventPage : Page
+    {
+        private Entities db = new Entities();
+        public EventPage()
+        {
+            InitializeComponent();
+            LoadEvents();
+        }
+        private void LoadEvents()
+        {
+            lstEvents.ItemsSource = db.Event.ToList();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new AuthPage());
+        }
+    }
+}
